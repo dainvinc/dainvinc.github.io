0916c8d0123bfd8bd9ca0598cd889ca81837cff6
Vishal <vishal.koosuri1@marist.edu>
2017-05-19 17:51:15 +0000

    Github link to open in new tab

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     if(window.location.pathname == '/about.html') {
         alert("Click to know more about Vishal!");    
     }
+    
+    $(".username").click(function() {
+       $(this).target = "_blank";
+    });
 });
