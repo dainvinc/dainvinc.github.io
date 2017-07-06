54a1b9f9565b41b86491b3edbcb79c3cf1f8c6a3
Vishal <vishal.koosuri1@marist.edu>
2017-07-06 14:57:28 +0000

    Capitalized the T in typed

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 //     });
 
 setTimeout(function(){
-        $(".site-title").typed({
+        $(".post-title").Typed({
             strings: ["Welcome..."],
             typeSpeed: 30, 
             backDelay: 750,
