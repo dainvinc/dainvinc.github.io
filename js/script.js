3418fa1da8d34d1745c439565119b17cd3219f04
Vishal <vishal.koosuri1@marist.edu>
2017-07-06 13:42:28 +0000

    Added a new typed script

--- js/script.js
+++ js/script.js
@@ -13,9 +13,13 @@ $(document).ready(function() {
 //         $(".site-title").toggle("slow", "linear");
 //     });
     
-$(function() {
-    $(".site-title").typed({strings: ["First sentence", "Second"], 
-        typeSpeed: 50, 
-        backDelay: 500
-    });
-})
+setTimeout(function(){
+        $(".element0").typed({
+            strings: ["apt-get install konquer"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 0);
